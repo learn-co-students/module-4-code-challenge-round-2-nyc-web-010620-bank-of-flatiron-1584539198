a7814160c1e4ef97bbd78087a24b0b5afc8c7425
Kate Raskauskas <kateapault@gmail.com>
2020-03-18 10:15:41 -0400

    notes on general strategy added, AccountContainer is correctly fetching transactions from db. Currently just console logging them

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,9 +2,25 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const TRANSACTURL = 'http://localhost:6001/transactions'
 
 class AccountContainer extends Component {
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+    fetch(TRANSACTURL)
+    .then(resp => resp.json())
+    .then(transactJSON => {
+      this.setState({
+        transactions: transactJSON
+      })
+    })
+  }
+
   render() {
+    console.log(`current state transactions: ${this.state.transactions}`)
     return (
       <div>
         <Search />
