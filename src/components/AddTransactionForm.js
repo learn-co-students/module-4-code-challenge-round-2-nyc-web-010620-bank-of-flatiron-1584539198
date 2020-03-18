60cd6a71761d8e6b71614c947f76cfa88044b6b4
Jay Wen <jaywen89@gmail.com>
2020-03-18 11:00:11 -0400

    adds function to handle form

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,32 +1,48 @@
 import React, { Component } from "react";
 
-const intialState = { date: "", description: "", category: "", amount: ""}
+const initialState = { date: "", description: "", category: "", amount: ""}
 
 class AddTransactionForm extends Component {
 
-  state = intialState
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault()
-  //   fetch('http://localhost:3000/transactions', {
-  //     method: "POST",
-  //     headers: {
-  //       "content-type" : "application/json",
-  //       accept: 'application/json'
-  //     }
-  //     )
-  // }
+  state = initialState
 
 
+  handleInput = (event) => {
+    const {name, value} = event.target
+    this.setState({[name]: value})
+  }
+
+  handleSubmit = (event) =>{
+  event.preventDefault()
+
+    fetch("http://localhost:3000/transactions", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify(addTransaction)
+    })
+    .then(response => response.json())
+    .then(transaction => {
+      this.props.addTransaction(transaction)
+    this.setState(initialState)
+    })
+    }
+
+
+
+  
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onsubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date}/>
+            <input type="text" name="description" value={this.state.description} placeholder="Description" />
+            <input type="text" name="category" value={this.state.category} placeholder="Category" />
             <input
+            value={this.state.amount}
               type="number"
               name="amount"
               placeholder="Amount"
