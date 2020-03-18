7fb834d8ea198bae5fc90ffb080a9146edc9232e
adam-xiao <adam.x.xiao@gmail.com>
2020-03-18 11:01:17 -0400

    done with base deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,42 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  
+  state={
+    transactions: [],
+    searchTerm: ""
+  }
+  
+  componentDidMount(){
+    fetch("http://localhost:6001/transactions")
+      .then(resp => resp.json())
+      .then(transactions => this.setState( {transactions} ))
+  }
+
+  handleOnSubmit=(transaction)=>{
+    let formattedTransaction = {...transaction, amount: parseInt(transaction.amount)}
+
+    fetch("http://localhost:6001/transactions", {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(formattedTransaction)
+    })
+  }
+
+  handleSearch=(e)=>{
+    this.setState( {searchTerm: e.target.value})
+    console.log(this.state.searchTerm)
+  }
+  
   render() {
+
+    let filteredTransactions = this.state.transactions.filter(transaction => transaction.description.includes(this.state.searchTerm))
+    //IS CASE SENSITIVE
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch}/>
+        <AddTransactionForm handleOnSubmit={this.handleOnSubmit}/>
+        <TransactionsList transactions={filteredTransactions}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  
+  state={
+    date: "",
+    description: "",
+    category: "",
+    amount: 0
+  }
+
+  handleOnChange=(e)=>{
+    this.setState( {[e.target.name]: e.target.value})
+    // this.setState( {amount: parseInt(e.target.value)} )
+  }
+  
   render() {
     return (
-      <div className="ui segment">
+      <div className="ui segment" onSubmit={() => this.props.handleOnSubmit(this.state)}>
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.handleOnChange}/>
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleOnChange}/>
+            <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleOnChange}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              value={this.state.amount}
+              onChange={this.handleOnChange}
             />
           </div>
           <button className="ui button" type="submit">
