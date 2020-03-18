f641b1144a475df3ca3d1228f31031f3e4b28cbe
Kate Raskauskas <kateapault@gmail.com>
2020-03-18 11:35:44 -0400

    added crowns to banner & added account total to display

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -60,12 +60,23 @@ class AccountContainer extends Component {
     }
   }
 
+  acctTotal = () => {
+    let n = 0
+    this.state.transactions.forEach(t => n += t.amount)
+    return n
+  }
+
   render() {
     return (
       <div>
         <Search handleChange={this.handleSearchChange} />
         <button onClick={this.handleClickSort}>Sort By Description</button>
         <AddTransactionForm addTransaction={this.addTransaction} />
+        <div>
+          Account total: {
+            Math.floor(this.acctTotal()*100)/100
+          }
+        </div>
         <TransactionsList transactions={this.state.displayedTransactions}/>
       </div>
     );
